c41d7aaab78ab88e33713c2f3ed6c667ddebef2b
marcxs2 <bichitoraptor22@gmail.com>
2022-03-10 12:16:01 +0100

    primera version

---
+++ codigo.java
@@ -0,0 +1,14 @@
+public static boolean HyperPar(int num){
+    int resto; 
+    while(num!=0)
+    {
+        resto= num%10;
+        num=num/10;
+        if(resto%2!=0)
+        {
+            return false;
+        }
+    
+    }
+    return true;
+}
